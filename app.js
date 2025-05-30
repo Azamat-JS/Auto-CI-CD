8e772987b12cee1a7c3ec969d0279d764545a79d
Azamat-JS <azamatielts987@gmail.com>
2025-05-30 19:34:12 +0500

    adding workflow with pm2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.get('/names', (req, res) => {
-    res.send('Node.js app running')
+    res.send('My app is running')
 });
 
 app.listen(5000, () => {
